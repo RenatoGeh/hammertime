c7b5dcc486924ce2fbf8dac94f92453ec49aefc5
Yan Couto <yancouto@gmail.com>
2013-05-24 15:50:27 -0300

    IT'S ALIVE

---
+++ game/hammertime.h
@@ -0,0 +1,9 @@
+#ifndef HAMMERTIME
+#define HAMMERTIME
+void load();
+
+int main(int argn, char ** args) {
+	load(argn, args);
+}
+
+#endif
---
+++ game/main.c
@@ -0,0 +1,6 @@
+#include "hammertime.h"
+#include <stdio.h>
+
+void load(int argn, char **args) {
+	printf("%d", argn);
+}
