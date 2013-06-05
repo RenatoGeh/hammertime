e93274d4894c24f4f7d73678e52c078ded8fe22f
Renato Lui Geh <renatogeh@gmail.com>
2013-06-05 18:16:30 -0300

    Moar intro.

--- game/intro.c
+++ game/intro.c
@@ -36,6 +36,11 @@ void drawBoard(int page) {
 			registerBody(NULL, newText(40, 12, "    Welcome to Hammertime, a text-based \"visual\" framework, made with and functional with the language C.", 60));
 			registerBody(NULL, newText(40, 15, "    The original idea was to create a simple yet useful     framework to use in projects using C, that didn't require   much knowledge in programming", 60));
 
+			registerBody(NULL, newText(40, 19, "    This little guide will help you with the basics of      *hammertime*. The window on the left indicates the page     you're on. To proceed to another page, type its number and  press enter", 60));
+			registerBody(NULL, newText(40,27, " For more information, questions or a more detailed guide, check our wiki at:",0));
+
+			registerBody(NULL, newText(45, 30, "https://github.com/RenatoGeh/hammertime/wiki", 0));
+
 			registerBody(NULL, newHeader(60, 1, "INTRODUCTION", -1, '!'));
 		break;
 	}
@@ -44,9 +49,9 @@ void drawBoard(int page) {
 void handleInput(int *page, char* input) {
 	if(input[0]>='0' && input[0]<='9')
 		*page=input[0]-'0';
-	else if(!strcmp(input, "next"))
+	else if(!strcmp(input, "next") || (input[0]=='n'&&input[1]=='\0'))
 		(*page)++;
-	else if(!strcmp(input, "prev"))
+	else if(!strcmp(input, "prev") || (input[0]=='p'&&input[1]=='\0'))
 		(*page)--;
 }
 
