dd91add7e9460149de056ad367d3210f2c05923f
Renato Lui Geh <renatogeh@gmail.com>
2013-06-05 19:23:20 -0300

    Attempting word wrap.

--- game/intro.c
+++ game/intro.c
@@ -1,18 +1,18 @@
 #include "hammertime.h"
 
-Body *newHeader(int x, int y, char *text, int wrap, char c) {
-	Body *b = newTextBox(x, y, text, wrap, c);
+Body *newHeader(int x, int y, char *text, int wrap) {
+	Body *b = newTextBox(x, y, text, wrap, ' ');
 	Stroke *s = newStroke(4, newBorder("="), newBorder("+"), newBorder("="), newBorder("+"));
 	b->addStroke(b,s);
 	setStrokeJoint(s, '+');
 	return b;
 }
 
-void drawSide(int page) {
+int drawSide(int page) {
 	int i, n=3;
-	char *contents[] = {"Introduction", "Bla", "Bla2"};
+	char *contents[] = {"Introduction", "Bodies", "Tables"};
 
-	registerBody(NULL, newTextBox(10, 1, " *HAMMERTIME* ", -1, '#'));
+	registerBody(NULL, newTextBox(10, 1, "  HAMMERTIME  ", -1, '#'));
 	registerBody(NULL, newLine(2, 4, 150, 'h', '-'));
 	registerBody(NULL, newText(10, 5, "Table of Content", 0));
 	registerBody(NULL, newLine(10, 6, 16, 'h', '_'));
@@ -27,40 +27,50 @@ void drawSide(int page) {
 	}
 
 	registerBody("", newPoint(13, 10+2*page, '*'));
+	return n;
 }
 
 void drawBoard(int page) {
 	switch(page) {
 		case 0:
+			registerBody(NULL, newHeader(60, 1, "INTRODUCTION", -1));
+
 			registerBody(NULL, newText(65, 9, "STOP!", 0));
 			registerBody(NULL, newText(40, 12, "    Welcome to Hammertime, a text-based \"visual\" framework, made with and functional with the language C.", 60));
-			registerBody(NULL, newText(40, 15, "    The original idea was to create a simple yet useful     framework to use in projects using C, that didn't require   much knowledge in programming", 60));
+			registerBody(NULL, newText(40, 15, "    The original idea was to create a simple yet useful framework to use in projects using C, that didn't require much knowledge in programming", 60));
 
-			registerBody(NULL, newText(40, 19, "    This little guide will help you with the basics of      *hammertime*. The window on the left indicates the page     you're on. To proceed to another page, type its number and  press enter", 60));
+			registerBody(NULL, newText(40, 19, "    This little guide will help you with the basics of Hammertime. The window on the left indicates the page you're on. To proceed to another page, type its number and press enter or type 'prev' (p), 'next' (n).", 60));
 			registerBody(NULL, newText(40,27, " For more information, questions or a more detailed guide, check our wiki at:",0));
 
 			registerBody(NULL, newText(45, 30, "https://github.com/RenatoGeh/hammertime/wiki", 0));
+		break;
+		case 1:
+		break;
+		case 2:
+			registerBody(NULL, newHeader(60, 1, "Tables!", -1));
 
-			registerBody(NULL, newHeader(60, 1, "INTRODUCTION", -1, '!'));
+			registerBody(NULL, newText(45, 9, "Structure:", 0));
+			registerBody(NULL, newText(40, 12, "    Tables are matrices. They are used to separate the screen into various 'mini-screens', with each having one Body in  each square.", 60));
+			//registerBody(NULL, newText(40, 15, "    "))
 		break;
 	}
 }
 
-void handleInput(int *page, char* input) {
+void handleInput(int *page, int n, char* input) {
 	if(input[0]>='0' && input[0]<='9')
 		*page=input[0]-'0';
-	else if(!strcmp(input, "next") || (input[0]=='n'&&input[1]=='\0'))
+	else if(*page<n-1 && ((input[0]=='n'&&input[1]=='\0') || !strcmp(input, "next")))
 		(*page)++;
-	else if(!strcmp(input, "prev") || (input[0]=='p'&&input[1]=='\0'))
+	else if(*page>0 && ((input[0]=='p'&&input[1]=='\0') || !strcmp(input, "prev")))
 		(*page)--;
 }
 
 int run() {
-	int page = 0;
+	int page = 0, npage;
 	char *input = (char*)malloc(sizeof(char));
 
 	do {
-		drawSide(page);
+		npage = drawSide(page);
 		drawBoard(page);
 
 		Body *line = newLine(35, 1, 60, 'v', '-');
@@ -72,7 +82,7 @@ int run() {
 		draw();
 		gets(input);
 
-		handleInput(&page, input);
+		handleInput(&page, npage, input);
 		
 		clearAndRefresh();
 		clearBodies();
